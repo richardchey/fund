9062e6bcb9e32e7f1494d3dfd7db148061b87603
Richard Chen <richardchey@163.com>
2015-06-30 03:22:58 +0800

    兼容php5.5

--- src/FundAbstract.php
+++ src/FundAbstract.php
@@ -9,7 +9,7 @@ abstract class FundAbstract
     const CODE = 'null';
     const SHORT = 'null';
     const NAME = 'null';
-    const HEADERS = [
+    protected static $HEADERS = [
     	'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     	'User-Agent' => 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36'
     ];
@@ -18,7 +18,7 @@ abstract class FundAbstract
 
     protected function getResFromUrl($url)
     {
-    	$client = new GuzzleHttp\Client(['headers'=>self::HEADERS]);
+    	$client = new GuzzleHttp\Client(['headers'=>self::$HEADERS]);
         $res = $client->get($url);
         return trim($res->getBody());
     }
